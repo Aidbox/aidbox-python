c53a8339942bb6e483649d0eca4585443f830648
GenaRazmakhnin <gena.razmakhnin@gmail.com>
2023-10-09 15:55:33 +0200

    add py.typed to package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     description='',
     python_requires='>=3.7',
-    package_data={"base": ["py.typed"], "resource": ["py.typed"]},
+    package_data={"python-toolkit": ["py.typed"]},
     include_package_data=True,
     install_requires=[
         'requests>=2.31.0',
